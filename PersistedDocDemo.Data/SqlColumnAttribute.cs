de832e8ac937153455c3b9208db3ed9e09be79de
Alastair Crabtree <alastair.crabtree@bbc.com>
2016-03-29 02:21:56 +0100

    Improve attribute usage

--- PersistedDocDemo.Data/SqlColumnAttribute.cs
+++ PersistedDocDemo.Data/SqlColumnAttribute.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Dynamic;
 
 namespace PersistedDocDemo.Data
 {
+	/// <summary>
+    /// When applied to a property persisted by the SqlRepository the property will be mapped to an SQL column
+    /// rather than serialised into JSON in the data column
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple=false)]
     public class SqlColumnAttribute : Attribute
     {
      
